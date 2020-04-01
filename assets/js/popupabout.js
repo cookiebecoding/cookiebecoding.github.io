1f99e59736d35078ea6f2df99541d0420a1ccc7f
Cookie <46170256+cookiebecoding@users.noreply.github.com>
2020-04-01 22:06:37 +0200

    added portfolio

---
+++ assets/js/popupabout.js
@@ -0,0 +1,33 @@
+(function($) {
+
+	// Get the popupabout
+	var popupabout = document.getElementById("myresume");
+
+	// Get the button that opens the popupabout
+	var btnheader = document.getElementById("popupabout-triggerheader");
+	var btnfooter = document.getElementById("popupabout-triggerfooter");
+
+	// Get the <span> element that closes the popupabout
+	var closethepop = document.getElementsByClassName("closethepop")[0];
+
+	// When the user clicks the button, open the popupabout
+	btnheader.onclick = function() {
+		popupabout.style.display = "block";
+	}
+	btnfooter.onclick = function() {
+		popupabout.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the popupabout
+	closethepop.onclick = function() {
+		popupabout.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the popupabout, close it
+	window.onclick = function(event) {
+		if (event.target == popupabout) {
+			popupabout.style.display = "none";
+		}
+	}
+
+})(jQuery);
---
+++ assets/stories-descriptions.py
@@ -0,0 +1,9 @@
+# DEFAULT LENGTH
+len("Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.")
+
+
+# 2020-04-01 Covid-19
+len("UPDATED DAILY - The new Corona virus is impacting everyone's lives. But how is the current situation in Belgium?")
+
+# 2020-04-01 De tijdloze
+len('This music chart has been around for over 20 years and remains popular in Belgium? But has there been an evolution?')
